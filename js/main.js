ecf1b250a2f925ae34fd1c87c0e3ffeeffb76b74
gr423_elmva <elokhov.2015@mail.ru>
2024-04-22 09:45:34 +0700

    Привязка атрибутов

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg"
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
 
