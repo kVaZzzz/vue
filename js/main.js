7abbb34f6b55a93263922d5a5d0f053c023e4b1c
gr423_elmva <elokhov.2015@mail.ru>
2024-04-23 09:12:43 +0700

    Обработка событий

--- js/main.js
+++ js/main.js
@@ -1,35 +1,54 @@
 let app = new Vue({
     el: '#app',
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        delToCart() {
-            this.cart -= 1
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
-
-    },
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                variantSale: 1,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                variantSale: 0,
             }
         ],
-        cart: 0,
+
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         }
-    })
+
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            return this.variants[this.selectedVariant].variantSale
+        },
+
+
+    },
+})
